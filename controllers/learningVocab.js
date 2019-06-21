6df3e29e896911146f9ac40cff9e15e12d1a46d6
unknown <kberg1@email.arizona.edu>
2019-06-21 09:23:01 -0700

    changing seed

--- controllers/learningVocab.js
+++ controllers/learningVocab.js
@@ -12,42 +12,45 @@ module.exports = {
     res.render('login');
   },
 
-  login: async function(req,res){
-    const userArr = await knex("users").where({email: req.body.email})
-    var user = userArr[0];
-    if(user && user.password === req.body.password){
-        await knex("users")
-              .where({email: req.body.email})
-              .update({language: req.body.language.substr(0,2)})
-        let rows = await knex("users")
-                              .where({email: req.body.email})
-        let currUser=rows[0];
-        req.session.user = currUser;
-        req.session.save(err=>{
-          if(err){
-            console.error(err);
-          }
-          knex("words")
-          .where({user_id:req.session.user.id})
-          .where({language: req.session.user.language})
-          .increment('count',-5)
-          .then(()=>{
-            knex("words")
-            .where({user_id:req.session.user.id})
-            .where({language: req.session.user.language})
-            .where('count','<',0)
-            .update({count: 0})
-            .then(()=>{
-              res.redirect("/");
-            })
-            .catch(err=>console.log(err));
-          })
-          .catch(error=>console.log(error));
+  login: function(req,res){
+    knex("users").where({email: req.body.email})
+    .then((rows)=>{
+      var user = rows[0];
+      if(user && user.password === req.body.password){
+        knex("users").where({email: req.body.email}).update({
+          language: req.body.language.substr(0,2)
         })
-    }
-    else{
-      res.redirect("/");
-    }
+        .then(()=>{
+          knex("users")
+          .where({email: req.body.email})
+          .then(rows=>{
+            let currUser=rows[0];
+            req.session.user = currUser;
+            req.session.save(err=>{
+              if(err){
+                console.error(err);
+              }
+              knex("words")
+              .where({user_id:req.session.user.id})
+              .update({
+                status:"blue",
+                count:0
+              }).then(result=>{
+                res.redirect("/");
+              })
+              .catch(error=>console.log(error))
+            })
+          });
+        })
+        .catch(err=>console.log(err));
+      }
+      else{
+        res.redirect("/");
+      }
+    })
+    .catch(err=>{
+      console.log(err);
+    })
   },
 
   register: function(req,res){
@@ -84,9 +87,10 @@ module.exports = {
   trainingPage: async function(req,res){
     // get words from the database based on user's input word
     const testBank = await knex('words')
-                          .orderBy('count', 'asc')
+                          .orderBy('updated_at', 'asc')
                           .where({language: req.session.user.language})
                           .where({ user_id: req.session.user.id })
+                          .whereNot({status: 'green'})
     if(testBank.length==0){
         res.redirect('/')//may want to make a new route for this because they don't know why they got kicked out
     }
@@ -94,26 +98,22 @@ module.exports = {
       const greenWordsArr = await knex('words')
                                   .where({ user_id: req.session.user.id })
                                   .where({language: req.session.user.language})
-                                  .where('count','>',99)
+                                  .where({status: 'green'})
       const greenWords = greenWordsArr.length
       const yellowWordsArr = await knex('words')
                                   .where({ user_id: req.session.user.id })
                                   .where({language: req.session.user.language})
-                                  .where('count','<=',99)
-                                  .where('count','>', 50)
+                                  .where({status: 'yellow'})
       const yellowWords = yellowWordsArr.length
       const redWordsArr = await knex('words')
                                   .where({ user_id: req.session.user.id })
                                   .where({language: req.session.user.language})
-                                  .where('count','<=', 50)
+                                  .where({status: 'red'})
       const redWords = redWordsArr.length;
       const allWordsArr = await knex('words')
                                 .where({language: req.session.user.language})
                                 .where({ user_id: req.session.user.id })
       const totalWords = allWordsArr.length
-      if(redWords>0){
-        Math.random()
-      }
       res.render('train', { 
         translatedWord: testBank[0] , 
         greenWords: greenWords, 
@@ -137,10 +137,10 @@ module.exports = {
     const result = await knex("words")
                           .where({user_id: req.session.user.id})
                           .where({language: req.session.user.language})//this should load the page with only words from the current session language??? not working
-                          .catch(err=>console.log(err))
+
     const word =req.body.inputWord
     const language= req.session.user.language
-    const newWord = await translate(word, language).catch(err=>console.log(err))
+    const newWord = await translate(word, language)
 
     res.render('newWord', {translatedWord:newWord, engWord:word, dictionary:result, language: req.session.user.language})
 
@@ -159,7 +159,7 @@ module.exports = {
           translation: translatedWord,
           user_id: userID,
           language: req.session.user.language,
-          count: 50,
+          count: 0,
           status: 'blue'
         }).then(result=>{
           res.redirect('/newWord');
@@ -189,44 +189,67 @@ module.exports = {
   train: function(req,res){
       if (req.body.inputWord === req.body.answer ) {
         knex('words').where({id: req.body.hiddenWord}).then(rows=>{
-          knex('words')
-            .where({id: req.body.hiddenWord})
-            .update({
-              count: rows[0].count<=80 ? rows[0].count+20: 100,
+          if(rows[0].count==2){
+            knex('words').where({id: req.body.hiddenWord}).update({
+              count: rows[0].count+1,
+              status: 'green'
             })
             .update('updated_at', knex.fn.now())
-            .then(result=>{
-              req.session.save(function(err){
-                if(err){
-                  console.error(err);
-                }
-                res.redirect("/train");
-      
-              })
-            })
-            .catch(err=>console.log(err));        
-        })
-      }
-      else{
-        knex('words')
-          .where({id: req.body.hiddenWord})
-          .then(rows=>{
-            knex('words')
-              .where({id: req.body.hiddenWord})
-              .update({
-                count: rows[0].count>=10 ? rows[0].count-10: 0,
+            .catch(err=>console.log(err));
+          }
+          else if(rows[0].count==0){
+            if(rows[0].status=='blue'){
+              knex('words').where({id: req.body.hiddenWord}).update({
+                count: rows[0].count+1,
+                status: 'green'
               })
               .update('updated_at', knex.fn.now())
               .catch(err=>console.log(err));
-            req.session.save(function(err){
-              if(err){
-                console.error(err);
-              }
-              res.redirect("/train");
+            }
+            else{
+              knex('words').where({id: req.body.hiddenWord}).update({
+                count: rows[0].count+1,
+                status: 'yellow'
+              })
+            .update('updated_at', knex.fn.now())
+            .catch(err=>console.log(err));
+            }
+          }
+          else {
+            knex('words').where({id: req.body.hiddenWord}).update({
+              count: rows[0].count+1
             })
+            .update('updated_at', knex.fn.now())
+            .catch(err=>console.log(err));
+          }
+        })
+        .then(result=>{
+          req.session.save(function(err){
+            if(err){
+              console.error(err);
+            }
+            res.redirect("/train");
+  
           })
+        })
+        
+      }
+      else{
+        knex('words').where({id: req.body.hiddenWord}).then(rows=>{
+          knex('words').where({id: req.body.hiddenWord}).update({
+            count: 0,
+            status: 'red'
+          })
+          .update('updated_at', knex.fn.now())
           .catch(err=>console.log(err));
-          
+        })
+        .catch(err=>console.log(err));
+        req.session.save(function(err){
+          if(err){
+            console.error(err);
+          }
+          res.redirect("/train");
+        })
       }
   },
   logout: function(req,res){
